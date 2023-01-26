042307d5303a79c80996843c3155778662720ba4
KirillMar <re4r@bk.ru>
2023-01-26 20:23:45 +0300

    ten
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
@@ -9,6 +9,12 @@ public class Solution {
     public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int[][] ints : multiArray) {
+            for (int[] anInt : ints) {
+                for (int i : anInt) {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
