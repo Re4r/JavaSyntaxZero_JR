918b592f7216f2cf86d371c614a7ea6766e1883d
KirillMar <re4r@bk.ru>
2023-01-26 17:24:13 +0300

    seven
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -10,6 +10,11 @@ public class Solution {
     public static int[] array;
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        array = new int[n];
+
+
     }
 }
