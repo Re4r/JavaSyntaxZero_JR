160f51a7c9a48bd3c9b5fc947d41387e1bf3db95
KirillMar <re4r@bk.ru>
2023-01-26 19:47:00 +0300

    nine
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public static void main(String[] args) {
 
         for (int i = 0; i < result.length; i++) {
-            result[i] = new int[i];
+            result[i] = new int[i + 1];
         }
 
         for (int j = 0; j < result.length; j++) {
