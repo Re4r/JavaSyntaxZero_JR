f54fe73e65ddd1dad0245e3e2d8b1df43ca58360
KirillMar <re4r@bk.ru>
2023-01-26 16:57:17 +0300

    six
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -8,6 +8,25 @@ Reverse
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int[] store = new int[n];
+
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                store[i] = s.nextInt();
+            }
+        }
+
+        if (n % 2 != 0) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(store[j]);
+            }
+        } else {
+            for (int k = n - 1; k >= 0; k--) {
+                System.out.println(store[k]);
+            }
+        }
     }
 }
