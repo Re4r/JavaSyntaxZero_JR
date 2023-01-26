c924b1ee41a287d48ea2e433c536815abaedb5b8
KirillMar <re4r@bk.ru>
2023-01-26 17:14:25 +0300

    six
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -10,6 +10,22 @@ public class Solution {
     public static int[] array;
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
+        }
+
+        int min = array[0];
+
+        for (int j = 1; j < array.length; j++) {
+            if (array[j] < min) {
+                min = array[j];
+            }
+        }
+        System.out.println(min);
+
     }
 }
