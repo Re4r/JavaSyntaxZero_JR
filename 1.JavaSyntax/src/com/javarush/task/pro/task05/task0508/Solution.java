a93fc11a641871beee1046f3ae841e7995d83a85
KirillMar <re4r@bk.ru>
2023-01-26 18:14:24 +0300

    seven
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -16,6 +16,23 @@ public class Solution {
         strings = new String[6];
 
         for (int i = 0; i < strings.length; i++) {
+            strings[i] = s.nextLine();
+        }
+
+        for (int j = 0; j < strings.length; j++) {
+            if (strings[j] == null) continue;
+
+            String temp = new String(strings[j]);
+
+            for (int k = j + 1; k < strings.length; k++) {
+                if (temp.equalsIgnoreCase(strings[k])) {
+                    strings[j] = null;
+                    strings[k] = null;
+                }
+            }
+        }
+
+        for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
     }
