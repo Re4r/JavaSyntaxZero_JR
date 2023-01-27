1116af59aeb501be1344aa40457c65e61850f490
KirillMar <re4r@bk.ru>
2023-01-27 14:00:36 +0300

    fourteen
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class Solution {
 
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static int element = 5;
+    public static int element = 57;
 
     public static void main(String[] args) {
-        Arrays.sort(array);
-        int index = Arrays.binarySearch(array, element);
 
-        if (index != -1) {
-            System.out.println(true);
-        } else {
-            System.out.print(false);
-        }
+        int[] array2 = Arrays.copyOf(array, array.length);
+        Arrays.sort(array2);
+
+        int index = Arrays.binarySearch(array2, element);
+
+        System.out.println(index > 0);
     }
 }
