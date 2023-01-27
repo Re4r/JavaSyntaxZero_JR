93cdbf694848edb060aaf56fd792451d07a0e37c
KirillMar <re4r@bk.ru>
2023-01-27 18:22:37 +0300

    oneGameMinesweeper
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -13,9 +13,9 @@ public class MinesweeperGame extends Game {
     }
 
     private void createGame() {
-        for (int x = 0; x < SIDE; x++) {
-            for (int y = 0; y < SIDE; y++) {
-                gameField[x][y] = new GameObject(x,y);
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                gameField[y][x] = new GameObject(x,y);
                 setCellColor(x, y, Color.ORANGE);
             }
         }
