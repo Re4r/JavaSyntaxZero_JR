5f87ca0ff78f0b3f6901cc361539f63aaf66f7e8
KirillMar <re4r@bk.ru>
2023-01-29 19:02:20 +0300

    nineteen
    Signed-off-by: KirillMar <re4r@bk.ru>

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int a, int b) {
+
+    }
+
+    public static void universalMethod(boolean a, boolean b) {
+
+    }
+
+    public static void universalMethod(String a, String b) {
+
+    }
+
+    public static void universalMethod(int[] a, int[] b) {
+
+    }
+
+    public static void universalMethod(double a, double b) {
+
+    }
+
+    public static void universalMethod(double[] a, double[] b) {
+
+    }
+
+    public static void universalMethod(String[] a, String[] b) {
+
+    }
+
+    public static void universalMethod(boolean[] a, boolean[] b) {
+
+    }
+
+    public static void universalMethod(int[] x, String y) {
+
+    }
+
+    public static void universalMethod(boolean a, boolean[] u) {
+
+    }
+}
