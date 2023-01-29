e9d4dbf1499797cdca881c28657fd9cbc94b8216
KirillMar <re4r@bk.ru>
2023-01-29 18:44:51 +0300

    nineteen
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.pro.task06.task0605;
 Правильный порядок
 */
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -14,7 +16,11 @@ public class Solution {
     }
 
     public static void reverseArray(int[] array) {
-        //напишите тут ваш код
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
     }
 
     public static void printArray(int[] array) {
