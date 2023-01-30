a8596e4a647c966af467416e85b9878f9d2c79d6
KirillMar <re4r@bk.ru>
2023-01-31 00:11:46 +0300

    SnakeGame
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -1,13 +1,28 @@
 package com.javarush.games.snake;
 
+import com.javarush.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
+
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
+
     private List<GameObject> snakeParts = new ArrayList<>();
+
     public Snake(int x, int y) {
         snakeParts.add(new GameObject(x, y));
         snakeParts.add(new GameObject(x + 1, y));
-        snakeParts.add((new GameObject(x + 2, y)));
+        snakeParts.add(new GameObject(x + 2, y));
+    }
+
+    public void draw(Game game) {
+        for (int i = 0; i < snakeParts.size(); i++) {
+            GameObject part = snakeParts.get(i);
+            String sign = (i != 0) ? BODY_SIGN : HEAD_SIGN;
+            game.setCellValue(part.x, part.y, sign);
+        }
     }
 }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -3,8 +3,10 @@ package com.javarush.games.snake;
 import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
+
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
 
     @Override
     public void initialize() {
@@ -12,15 +14,17 @@ public class SnakeGame extends Game {
         createGame();
     }
 
-    private void createGame() {
+    private void createGame(){
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
     }
 
-    private void drawScene() {
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                setCellColor(x, y, Color.ANTIQUEWHITE);
+    private void drawScene(){
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellColor(i, j, Color.ANTIQUEWHITE);
             }
         }
+        snake.draw(this);
     }
 }
