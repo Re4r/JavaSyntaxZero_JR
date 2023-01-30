a455093e90231719c85c3e829c61dceb00b687a8
KirillMar <re4r@bk.ru>
2023-01-31 01:01:32 +0300

    SnakeGame
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -11,16 +11,12 @@ public class Snake {
     private static final String BODY_SIGN = "\u26AB";
     private Direction direction = Direction.LEFT;
     public boolean isAlive = true;
-
-
     private List<GameObject> snakeParts = new ArrayList<>();
-
     public Snake(int x, int y) {
         snakeParts.add(new GameObject(x, y));
         snakeParts.add(new GameObject(x + 1, y));
         snakeParts.add(new GameObject(x + 2, y));
     }
-
     public void draw(Game game) {
         Color color = isAlive ? Color.BLACK : Color.RED;
         for (int i = 0; i < snakeParts.size(); i++) {
@@ -29,12 +25,26 @@ public class Snake {
             game.setCellValueEx(part.x, part.y, Color.NONE, sign, color, 75);
         }
     }
-
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
-
     public void move() {
 
     }
+    public GameObject createNewHead() {
+        GameObject oldHead = snakeParts.get(0);
+        if (direction == Direction.LEFT) {
+            return new GameObject(oldHead.x - 1, oldHead.y);
+        } else if (direction == Direction.RIGHT) {
+            return new GameObject(oldHead.x + 1, oldHead.y);
+        } else if (direction == Direction.UP) {
+            return new GameObject(oldHead.x, oldHead.y - 1);
+        } else {
+            return new GameObject(oldHead.x, oldHead.y + 1);
+        }
+    }
+    public void removeTail() {
+        snakeParts.remove(snakeParts.size() - 1);
+    }
+
 }
