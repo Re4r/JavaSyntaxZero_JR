6d9ad3f1393c79ae1a66bd0a9e9461eb479febb9
KirillMar <re4r@bk.ru>
2023-02-01 15:19:28 +0300

    SnakeGame
    Signed-off-by: KirillMar <re4r@bk.ru>

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte x1 = 127;
+    short x2 = 32767;
+    int x3 = 2147483647;
+    long x4 = 9_223_372_036_854_775_807L;
+}
