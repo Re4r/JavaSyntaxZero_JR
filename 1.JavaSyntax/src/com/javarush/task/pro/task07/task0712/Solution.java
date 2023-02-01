4e80eb999c27b11244c341cd5d19cc3828310ded
KirillMar <re4r@bk.ru>
2023-02-01 18:14:37 +0300

    SnakeGame
    Signed-off-by: KirillMar <re4r@bk.ru>

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String one = new String("");
+        String two = new String("");
+        String three = new String("");
+        String four = new String("");
+        String five = new String("");
+        String six = new String("");
+        String seven = new String("");
+        String eight = new String("");
+        String nine = new String("");
+        String ten = new String("");
+
+        int[] x1 = new int[10];
+        int[] x2 = new int[10];
+        int[] x3 = new int[10];
+        int[] x4 = new int[10];
+        int[] x5 = new int[10];
+
+        Scanner s1 = new Scanner(System.in);
+        Scanner s2 = new Scanner(System.in);
+    }
+}
