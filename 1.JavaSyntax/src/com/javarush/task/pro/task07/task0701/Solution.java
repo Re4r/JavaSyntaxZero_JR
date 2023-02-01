c263791d87890ac9b0c5f43af875898397f93af4
KirillMar <re4r@bk.ru>
2023-02-01 14:57:21 +0300

    SnakeGame
    Signed-off-by: KirillMar <re4r@bk.ru>

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte x1;
+    short x2;
+    int x3;
+    long x4;
+    float x5;
+    double x6;
+    boolean x7;
+    char x8;
+}
