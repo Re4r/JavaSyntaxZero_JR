ad82ec978e66324e43056b052aa48f05a1c9d2ec
KirillMar <re4r@bk.ru>
2023-02-02 13:57:31 +0300

    Game2048
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -20,9 +20,9 @@ public class Game2048 extends Game {
         createNewNumber();
     }
     private void drawScene() {
-        for (int x = 0; x < gameField.length; x++) {
-            for (int y = 0; y < gameField[x].length; y++) {
-                setCellColor(x, y, Color.RED);
+        for (int x = 0; x < SIDE; x++) {
+            for (int y = 0; y < SIDE; y++) {
+                setCellColoredNumber(x, y, gameField[y][x]);
             }
         }
     }
@@ -38,6 +38,26 @@ public class Game2048 extends Game {
         } while (!isCreated);
     }
     private Color getColorByValue(int value) {
+        switch (value) {
+            case 0: return Color.WHITE;
+            case 2: return Color.PLUM;
+            case 4: return Color.SLATEBLUE;
+            case 8: return Color.DODGERBLUE;
+            case 16: return Color.DARKTURQUOISE;
+            case 32: return Color.MEDIUMSEAGREEN;
+            case 64: return Color.LIMEGREEN;
+            case 128: return Color.DARKORANGE;
+            case 256: return Color.SALMON;
+            case 512: return Color.ORANGERED;
+            case 1024: return Color.DEEPPINK;
+            case 2048: return Color.MEDIUMVIOLETRED;
+            default: return Color.NONE;
+        }
+    }
+    private void setCellColoredNumber(int x, int y, int value) {
+        Color color = getColorByValue(value);
+        String str = value > 0 ? "" + value : "";
+        setCellValueEx(x, y, color, str);
 
     }
 
