ad1d15d32fa71f7496c5a0d03673a421351b033d
KirillMar <re4r@bk.ru>
2023-02-02 15:55:27 +0300

    Game2048
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -8,6 +8,7 @@ public class Game2048 extends Game {
     private static final int SIDE = 4;
     private int[][] gameField = new int[SIDE][SIDE];
     private boolean isGameStopped = false;
+    private int score;
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
@@ -89,6 +90,8 @@ public class Game2048 extends Game {
                 row[i] += row[i + 1];
                 row[i + 1] = 0;
                 result = true;
+                score += row[i];
+                setScore(score);
             }
         }
         return result;
@@ -98,6 +101,8 @@ public class Game2048 extends Game {
         if (isGameStopped) {
             if (key == Key.SPACE) {
                 isGameStopped = false;
+                score = 0;
+                setScore(score);
                 createGame();
                 drawScene();
             } else {
