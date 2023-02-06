4226717df2c84516a2d72dc8d119c731e26e2279
KirillMar <re4r@bk.ru>
2023-02-06 13:58:36 +0300

    11_level
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1003/Skyscraper.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1003/Skyscraper.java
@@ -8,7 +8,14 @@ public class Skyscraper {
     private int floorsCount;
     private String developer;
 
-    //напишите тут ваш код
+    public Skyscraper() {
+        floorsCount = 5;
+        developer = "JavaRushDevelopment";
+    }
+    public Skyscraper(int floorsCount, String developer) {
+        this.floorsCount = floorsCount;
+        this.developer = developer;
+    }
 
     public static void main(String[] args) {
         Skyscraper skyscraper = new Skyscraper();
