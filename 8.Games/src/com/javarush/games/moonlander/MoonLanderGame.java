8efff474be3b65f1a64dc50eb25a3e471db0b460
KirillMar <re4r@bk.ru>
2023-02-08 18:18:56 +0300

    MoonLanderGame
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -9,5 +9,16 @@ public class MoonLanderGame extends Game {
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+    private void createGame() {
+        drawScene();
+    }
+    private void drawScene() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                setCellColor(x, y, Color.DEEPSKYBLUE);
+            }
+        }
     }
 }
