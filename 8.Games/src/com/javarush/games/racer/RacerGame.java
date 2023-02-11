df38d6991b23f2fad9225ed8ff9786f0ab47abe2
KirillMar <re4r@bk.ru>
2023-02-11 11:22:17 +0300

    RacerGame
    Signed-off-by: KirillMar <re4r@bk.ru>

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -1,4 +1,14 @@
 package com.javarush.games.racer;
 
-public class RacerGame {
+import com.javarush.engine.cell.*;
+
+public class RacerGame extends Game {
+    public static final int WIDTH = 64;
+    public static final int HEIGHT = 64;
+
+    @Override
+    public void initialize() {
+        showGrid(false);
+        setScreenSize(WIDTH, HEIGHT);
+    }
 }
