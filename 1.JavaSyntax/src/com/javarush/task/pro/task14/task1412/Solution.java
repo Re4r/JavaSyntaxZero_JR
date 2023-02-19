abf02680a19bf269fa32d0205f01ff86222f8fc6
KirillMar <re4r@bk.ru>
2023-02-19 19:20:42 +0300

    15_level

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1412/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task14.task1412;
+
+/* 
+Группировка исключений
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+
+        } catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+
+        } catch (InterruptedException | IllegalArgumentException | IllegalStateException e) {
+            System.out.println("Произошло исключение на букву I");
+        }
+    }
+}
