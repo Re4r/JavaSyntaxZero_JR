c94cf09d97911164be0fe79fc62558ef0517bc55
KirillMar <re4r@bk.ru>
2023-02-21 20:32:58 +0300

    16_level

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
@@ -13,9 +13,11 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            List<String> lines = Files.readAllLines(Paths.get(scanner.nextLine()));
+            List<String> lines = Files.readAllLines(Path.of(scanner.nextLine()));
+            System.out.println(lines);
             lines.forEach(str -> {
                 char[] chars = str.toCharArray();
+                System.out.println(chars);
                 for (char character : chars) {
                     if (character != ' ' && character != '.' && character != ',') {
                         System.out.print(character);
@@ -24,6 +26,7 @@ public class Solution {
             });
         } catch (Exception e) {
             System.out.println("Something went wrong : " + e);
+            e.printStackTrace();
         }
     }
 }
