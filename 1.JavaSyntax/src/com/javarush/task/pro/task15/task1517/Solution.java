5e4c94560bc6fbd49429ebdef92963576066061a
KirillMar <re4r@bk.ru>
2023-02-24 01:48:00 +0300

    16_level

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1517/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1517/Solution.java
@@ -14,7 +14,15 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         Path filePath = Path.of(scanner.nextLine());
         Path fileNewPath = Path.of(scanner.nextLine());
-        //напишите тут ваш код
+
+       if (Files.notExists(filePath)) Files.createFile(filePath);
+       if (Files.exists(filePath)) {
+           if (Files.notExists(fileNewPath)) {
+               Files.move(filePath, fileNewPath);
+           } else if (Files.exists(fileNewPath)) {
+               Files.delete(filePath);
+           }
+       }
     }
 }
 
