3a1656ef5d7b8f3aa2e6b36c4175ce076e3c883b
KirillMar <re4r@bk.ru>
2023-02-25 21:25:19 +0300

    17_level

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1601/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1601/Solution.java
@@ -3,21 +3,37 @@ package com.javarush.task.pro.task16.task1601;
 import java.util.Calendar;
 import java.util.Date;
 
-/* 
+/*
 Лишь бы не в понедельник :)
 */
 
 public class Solution {
 
-    static Date birthDate;
+    static Date birthDate = new Date(120, Calendar.MARCH, 17);
 
     public static void main(String[] args) {
         System.out.println(getDayOfWeek(birthDate));
     }
 
     static String getDayOfWeek(Date date) {
-        //напишите тут ваш код
-
-        return null;
+        String result = "";
+        int day = date.getDay();
+        switch (day) {
+            case 0:
+                return "воскресенье";
+            case 1:
+                return "понедельник";
+            case 2:
+                return "вторник";
+            case 3:
+                return "среда";
+            case 4:
+                return "четверг";
+            case 5:
+                return "пятница";
+            case 6:
+                return "суббота";
+        }
+        return result;
     }
 }
