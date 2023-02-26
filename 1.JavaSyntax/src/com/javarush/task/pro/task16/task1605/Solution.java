a1757f7873ee965ae96e01e8519f00c323d201fa
KirillMar <re4r@bk.ru>
2023-02-26 14:51:51 +0300

    17_level

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1605/Solution.java
@@ -16,12 +16,19 @@ public class Solution {
 
     public static void main(String[] args) {
         Calendar currentDay = Calendar.getInstance();
-        //напишите тут ваш код
+        while (!isTeleportInvented(currentDay)) {
+            currentDay.add(Calendar.YEAR, 10);
+        }
     }
 
     static boolean isTeleportInvented(Calendar currentDay) {
-        //напишите тут ваш код
-
+        if (currentDay.after(INVENTION_DAY)) {
+            System.out.println("Пожалуйста, можете забрать ваш карманный телепорт. Спасибо за ожидание!");
+            return true;
+        } else if (currentDay.before(INVENTION_DAY)) {
+            System.out.println("Извините, телепорт еще не изобрели, приходите через 10 лет.");
+            return false;
+        }
         return false;
     }
 }
